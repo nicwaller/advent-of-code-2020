1da1f5eaddc9bd3252d129cc76425526fa78ee45
Nic Waller <code@nicwaller.com>
2020-12-06 22:16:59 -0800

    day 7 part 2

--- aoc2020/day07.swift
+++ aoc2020/day07.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct BagRule {
     var bagColour: BagColour;
-    var mayContain: Dictionary<BagColour, Int> = [:];
+    var mustContain: Dictionary<BagColour, Int> = [:];
 }
 
 typealias Ruleset = Dictionary<BagColour, BagRule>
@@ -18,31 +18,29 @@ typealias BagColour = String
 
 func parseBagRule(_ text: String) -> BagRule {
     let parts = text.components(separatedBy: "contain").map{ $0.trimmingCharacters(in: .whitespaces) }
-    var rule = BagRule(bagColour: parts[0].replacingOccurrences(of: " bags", with: ""), mayContain: [:])
+    var rule = BagRule(bagColour: parts[0].replacingOccurrences(of: " bags", with: ""), mustContain: [:])
     let containsTxt: String = parts[1]
+    if (containsTxt == "no other bags.") {
+        return rule;
+    }
     let containsTxts: [String] = containsTxt.components(separatedBy: ",").compactMap{ $0.trimmingCharacters(in: .whitespaces).replacingOccurrences(of: ".", with: "").replacingOccurrences(of: "bags", with: "bag") .replacingOccurrences(of: " bag", with: "") }
     
     for cT in containsTxts {
         let s = String(cT)
         let key = String(s[s.index(s.startIndex, offsetBy: 2)...])
-        rule.mayContain[key] = Int(String(cT.first!))
+        rule.mustContain[key] = Int(String(cT.first!))
     }
     return rule
-//    var rule = BagRule(bagColour: parts[0], );
 }
 
-// look up a bag, find what bags may contain it
 var invertedIndex: Dictionary<BagColour, Set<BagColour>> = [:]
 
 func bagsThatCanDeepContain(colour: BagColour, visited: inout Set<BagColour>) -> Set<BagColour> {
-    print("Visiting \(colour)")
-
     if (visited.contains(colour)) {
         return Set();
     }
 
     guard let parents: Set<BagColour> = invertedIndex[colour] else {
-        print(colour)
         return Set()
     }
     let result = parents
@@ -52,37 +50,43 @@ func bagsThatCanDeepContain(colour: BagColour, visited: inout Set<BagColour>) ->
     return result;
 }
 
+var ruleDict: Ruleset = [:]
+
+func containedBags(colour: BagColour) -> Int {
+    if (ruleDict[colour]?.mustContain.count == 0) {
+        return 0;
+    } else {
+        let inner = ruleDict[colour]!
+        let innerCounts = inner.mustContain.map{ (1 + containedBags(colour: $0)) * $1 }
+        return innerCounts.reduce(0, +)
+    }
+}
+
 func day07() {
     day07test()
-//    let rules = day7puzzleInput.components(separatedBy: .newlines).map{ parseBagRule($0) }
-//    var ruleDict: Ruleset = [:]
     
-
     for ruleStr in day7puzzleInput.components(separatedBy: .newlines) {
         let rule = parseBagRule(ruleStr)
-        for c in rule.mayContain.keys {
+        for c in rule.mustContain.keys {
             if (!invertedIndex.keys.contains(c)) {
                 invertedIndex[c] = Set()
             }
             invertedIndex[c]?.insert(rule.bagColour)
         }
+        ruleDict[rule.bagColour] = rule
+        if rule.mustContain.keys.count == 0 {
+            print(rule)
+        }
     }
-//    print(invertedIndex)
     let myBagColour = "shiny gold"
     var visited: Set<BagColour> = Set()
     let deepParents = bagsThatCanDeepContain(colour: myBagColour, visited: &visited)
-    print(deepParents)
-    print(deepParents.count)
     assert(248 == deepParents.count)
-    
-//    print(rules)
+    print(deepParents.count)
 
-//    assert(987 == seats.max())
-//    print("Highest: \(seats.max()!)")
-//
-//    let lastOfSeq = lastInSequence(seats.sorted())
-//    assert(602 == lastOfSeq)
-//    print("Missing: \(lastOfSeq + 1)")
+    let deepChildren = containedBags(colour: myBagColour)
+    assert(57281 == deepChildren)
+    print(deepChildren)
 }
 
 func day07test() {
