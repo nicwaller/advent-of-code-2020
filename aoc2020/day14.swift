4741b6bb4919822327cd653fd55ac63cd49250ae
Nic Waller <code@nicwaller.com>
2020-12-13 22:08:44 -0800

    day 14 part 2

--- aoc2020/day14.swift
+++ aoc2020/day14.swift
@@ -29,16 +29,17 @@ class Day14: AOC {
 
     let input = parse(day14puzzleinput)
     let example = parse("""
-mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
-mem[8] = 11
-mem[7] = 101
-mem[8] = 0
+mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1
 """)
     
     func run(_ parts: [Directive]) -> Dictionary<Int, Int> {
         var memory: Dictionary<Int, Int> = [:]
-        var offMask = 0b111111111111111111111111111111111111
+        // var offMask = 0b111111111111111111111111111111111111
         var onMask = 0b000000000000000000000000000000000000
+        var xorMasks: [Int] = [0]
         for inst in parts {
             if inst.0 == "" {
                 let addr = inst.1
@@ -46,29 +47,76 @@ mem[8] = 0
                 if !memory.keys.contains(addr) {
                     memory[addr] = 0
                 }
-                memory[addr] = (operand | onMask) & offMask
+                for x in xorMasks {
+                    let effectiveAddr = (addr | onMask) ^ x
+                    memory[effectiveAddr] = operand
+                }
             } else {
                 let newMask: String = inst.0
-                offMask = Int(newMask.replace("X", "1"), radix: 2)!
+//                offMask = Int(newMask.replace("X", "1"), radix: 2)!
                 onMask = Int(newMask.replace("X", "0"), radix: 2)!
+                xorMasks = compiledFloatMasks(newMask.replace("1", "0"))
             }
         }
         return memory
+    }
+    
+//    func permute<X>(_ list: [X]) -> [(X, X)] {
+//        if list.count == 0 {
+//            return []
+//        } else if list.count == 1 {
+//            return [(list[0], list[0])]
+//        } else {
+//            let tail = list[1 ..< list.count]
+//            let sub = permute(tail)
+//
+//        }
+//    }
+    
+    // returns a set of bit masks to be XORed with the real memory address
+    func floatMasks(mask: String) -> [String] {
+//        var masks: [Int] = []
+//        for (bitIndex, c) in mask.utf8.reversed().enumerated() {
+//            if c == 88 /* X=88 */ {
+//                masks.append(2 << (bitIndex-1))
+//                print(c)
+//            }
+//        }
+//        return masks
+        if mask == "" {
+//            print("impossible!")
+            return [""]
+        } else if mask.hasPrefix("X") {
+            let (_, remainder) = mask.take(1)
+            let tails = floatMasks(mask: remainder)
+            return tails.map{ "0" + $0 } + tails.map{ "1" + $0 }
+        } else {
+            let (c, remainder) = mask.take(1)
+            let tails = floatMasks(mask: remainder)
+            return tails.map{ c + $0 }
+        }
+    }
+    
+    func compiledFloatMasks(_ mask: String) -> [Int] {
+        return floatMasks(mask: mask).map{ Int($0, radix: 2)! }
     }
 
     func test() {
         print(example)
         let A = run(example)
-        print(A.values.reduce(0, +))
+        print(A.values)
 //        assert(0 != A)
     }
     
     func part1() -> Void {
-        let A = run(input).values.reduce(0, +)
-        print("Part 1: \(A)")
+//        let A = run(input).values.reduce(0, +)
+//        print("Part 1: \(A)")
 //        assert(0 != A)
     }
     
     func part2() -> Void {
+        let A = run(input).values.reduce(0, +)
+        print("Part 2: \(A)")
+//        assert(0 != A)
     }
 }
