763a4fda75be34b035214e2f8ae1e5326137634c
Nic Waller <code@nicwaller.com>
2020-12-13 21:22:01 -0800

    day 14 part 1

--- aoc2020/day14.swift
+++ aoc2020/day14.swift
@@ -9,28 +9,64 @@ import Foundation
 
 class Day14: AOC {
     
-    static func parse(_ all: String) -> [String] {
+    typealias Directive = (String, Int, Int)
+    
+    static func parse(_ all: String) -> [Directive] {
         let lines = all.splitOn(.newlines)
-        return lines
+        var dirs: [Directive] = []
+        for line in lines {
+            let parts = line.splitOn(" = ")
+            if parts[0] == "mask" {
+                dirs.append((parts[1], -1, -1))
+            } else {
+                let memAddrTxt = parts[0].replace("[", "").replace("]", "")
+                let (_, addr) = memAddrTxt.take(3)
+                dirs.append(("", Int(addr)!, Int(parts[1])!))
+            }
+        }
+        return dirs
     }
 
     let input = parse(day14puzzleinput)
     let example = parse("""
+mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+mem[8] = 11
+mem[7] = 101
+mem[8] = 0
 """)
     
-    func fun1(_ parts: [String]) -> Int {
-        return -1
+    func run(_ parts: [Directive]) -> Dictionary<Int, Int> {
+        var memory: Dictionary<Int, Int> = [:]
+        var offMask = 0b111111111111111111111111111111111111
+        var onMask = 0b000000000000000000000000000000000000
+        for inst in parts {
+            if inst.0 == "" {
+                let addr = inst.1
+                let operand = inst.2
+                if !memory.keys.contains(addr) {
+                    memory[addr] = 0
+                }
+                memory[addr] = (operand | onMask) & offMask
+            } else {
+                let newMask: String = inst.0
+                offMask = Int(newMask.replace("X", "1"), radix: 2)!
+                onMask = Int(newMask.replace("X", "0"), radix: 2)!
+            }
+        }
+        return memory
     }
 
     func test() {
-        let A = fun1(example)
-        assert(0 != A)
+        print(example)
+        let A = run(example)
+        print(A.values.reduce(0, +))
+//        assert(0 != A)
     }
     
     func part1() -> Void {
-        let A = fun1(input)
+        let A = run(input).values.reduce(0, +)
         print("Part 1: \(A)")
-        assert(0 != A)
+//        assert(0 != A)
     }
     
     func part2() -> Void {
