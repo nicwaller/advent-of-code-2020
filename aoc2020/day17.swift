d558620c62714e70ad63ed291b4dd36558ff02f0
Nic Waller <code@nicwaller.com>
2020-12-16 21:36:29 -0800

    day 17 part 2

--- aoc2020/day17.swift
+++ aoc2020/day17.swift
@@ -9,33 +9,35 @@ import Foundation
 
 class Day17: AOC {
     
-    struct Point3D: Hashable {
+    struct Point4D: Hashable {
         var x: Int
         var y: Int
         var z: Int
+        var w: Int
     }
 
-    typealias Bounds3D = (ClosedRange<Int>, ClosedRange<Int>, ClosedRange<Int>)
-    typealias Cube = Set<Point3D>
+    typealias Bounds4D = (ClosedRange<Int>, ClosedRange<Int>, ClosedRange<Int>, ClosedRange<Int>)
+    typealias Cube = Set<Point4D>
     
     static func parse(_ all: String) -> Cube {
         var C: Cube = Set()
         for (y, row) in all.splitOn(.newlines).enumerated() {
             for (x, char) in row.enumerated() {
                 if char == "#" {
-                    C.insert(Point3D(x: x, y: y, z: 0))
+                    C.insert(Point4D(x: x, y: y, z: 0, w: 0))
                 }
             }
         }
         return C
     }
     
-    func calcBounds(_ C: Cube, extend: Int = 0) -> Bounds3D {
+    func calcBounds(_ C: Cube, extend: Int = 0) -> Bounds4D {
         let basis = C.first!
 
         var minX = basis.x, maxX = basis.x
         var minY = basis.y, maxY = basis.y
         var minZ = basis.z, maxZ = basis.z
+        var minW = basis.w, maxW = basis.w
         
         for P in C {
             minX = min(minX, P.x)
@@ -44,6 +46,8 @@ class Day17: AOC {
             maxY = max(maxY, P.y)
             minZ = min(minZ, P.z)
             maxZ = max(maxZ, P.z)
+            minW = min(minW, P.w)
+            maxW = max(maxW, P.w)
         }
         
         minX -= extend
@@ -52,21 +56,25 @@ class Day17: AOC {
         maxY += extend
         minZ -= extend
         maxZ += extend
-        
-        return ( minX ... maxX, minY ... maxY, minZ ... maxZ )
+        minW -= extend
+        maxW += extend
+
+        return ( minX ... maxX, minY ... maxY, minZ ... maxZ, minW ... maxW )
     }
     
-    func activeNeighbours(_ C: Cube, at: Point3D) -> Set<Point3D> {
-        var neighbours: Set<Point3D> = Set()
+    func activeNeighbours(_ C: Cube, at: Point4D) -> Set<Point4D> {
+        var neighbours: Set<Point4D> = Set()
         for x in at.x - 1 ... at.x + 1 {
             for y in at.y - 1 ... at.y + 1 {
                 for z in at.z - 1 ... at.z + 1 {
-                    let P = Point3D(x: x, y: y, z: z)
-                    if P == at {
-                        continue
-                    }
-                    if C.contains(P) {
-                        neighbours.insert(P)
+                    for w in at.w - 1 ... at.w + 1 {
+                        let P = Point4D(x: x, y: y, z: z, w: w)
+                        if P == at {
+                            continue
+                        }
+                        if C.contains(P) {
+                            neighbours.insert(P)
+                        }
                     }
                 }
             }
@@ -80,24 +88,26 @@ class Day17: AOC {
         for x in bounds.0 {
             for y in bounds.1 {
                 for z in bounds.2 {
-                    let P = Point3D(x: x, y: y, z: z)
-                    let neighbours = activeNeighbours(C, at: P)
-                    var newIsActive: Optional<Bool> = nil
-                    if C.contains(P) {
-                        if neighbours.count >= 2 && neighbours.count <= 3 {
-                            newIsActive = true
+                    for w in bounds.3 {
+                        let P = Point4D(x: x, y: y, z: z, w: w)
+                        let neighbours = activeNeighbours(C, at: P)
+                        var newIsActive: Optional<Bool> = nil
+                        if C.contains(P) {
+                            if neighbours.count >= 2 && neighbours.count <= 3 {
+                                newIsActive = true
+                            } else {
+                                newIsActive = false
+                            }
                         } else {
-                            newIsActive = false
+                            if neighbours.count == 3 {
+                                newIsActive = true
+                            } else {
+                                newIsActive = false
+                            }
                         }
-                    } else {
-                        if neighbours.count == 3 {
-                            newIsActive = true
-                        } else {
-                            newIsActive = false
+                        if newIsActive! {
+                            newWorld.insert(P)
                         }
-                    }
-                    if newIsActive! {
-                        newWorld.insert(P)
                     }
                 }
             }
@@ -106,28 +116,28 @@ class Day17: AOC {
     }
     
     // returns an array of planes
-    func renderCube(_ C: Cube) -> String {
-        let bounds = calcBounds(C)
-        print(bounds)
-        var planes: [String] = []
-        for z in bounds.2 {
-            var lines: [String] = []
-            for y in bounds.1 {
-                var line: [String] = []
-                for x in bounds.0 {
-                    let P = Point3D(x: x, y: y, z: z)
-                    if C.contains(P) {
-                        line.append("#")
-                    } else {
-                        line.append(".")
-                    }
-                }
-                lines.append(line.joined(separator: ""))
-            }
-            planes.append(lines.joined(separator: "\n"))
-        }
-        return planes.joined(separator: "\n\n")
-    }
+//    func renderCube(_ C: Cube) -> String {
+//        let bounds = calcBounds(C)
+//        print(bounds)
+//        var planes: [String] = []
+//        for z in bounds.2 {
+//            var lines: [String] = []
+//            for y in bounds.1 {
+//                var line: [String] = []
+//                for x in bounds.0 {
+//                    let P = Point4D(x: x, y: y, z: z)
+//                    if C.contains(P) {
+//                        line.append("#")
+//                    } else {
+//                        line.append(".")
+//                    }
+//                }
+//                lines.append(line.joined(separator: ""))
+//            }
+//            planes.append(lines.joined(separator: "\n"))
+//        }
+//        return planes.joined(separator: "\n\n")
+//    }
             
     func test() -> Void {
         let example = """
@@ -135,17 +145,24 @@ class Day17: AOC {
 ..#
 ###
 """
-        let P = Day17.parse(example)
-        print(renderCube(P))
+        var P = Day17.parse(example)
+//        print(renderCube(P))
+        for _ in 1 ... 6 {
+           P = advance(P)
+        }
+        assert(848 == P.count)
 
-        print("---")
-        let P1 = advance(P)
-        print(renderCube(P1))
+//        print("---")
+//        let P1 = advance(P)
+//        print(renderCube(P1))
 //        print(P)
-//        print("tests OK")
+        print("tests OK")
     }
 
     func part1() -> Void {
+    }
+    
+    func part2() -> Void {
         var P = Day17.parse(day17puzzleinput)
         print(P)
         for _ in 1 ... 6 {
@@ -153,12 +170,7 @@ class Day17: AOC {
         }
         print(P)
         let A = P.count
-        print("Part 1: \(A)")
-        assert(A != 325)
-    }
-    
-    func part2() -> Void {
-//        let A = -1
-//        print("Part 2: \(A)")
+        print("Part 2: \(A)")
+//        assert(A == 291)
     }
 }
